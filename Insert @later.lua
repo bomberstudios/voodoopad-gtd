7bbda86f8f7287bbd845b1e2323c217886c9c1c0
Ale Muñoz <bomberstudios@gmail.com>
2009-02-10 15:42:30 +0100

    New command: "Update @later List" and scripts to insert @todo, @done and @later tags.

---
+++ Insert @later.lua
@@ -0,0 +1,17 @@
+--[[
+VPLanguage = lua
+VPScriptSuperMenuTitle = GTD
+VPScriptMenuTitle = Insert @later
+VPShortcutKey = l
+VPShortcutMask = control
+VPEndConfig
+--]]
+
+-- check out http://www.lua.org/pil/22.1.html for more formatting options.
+
+textView = windowController:textView()
+document = windowController:document()
+
+if textView ~= nil then
+    textView:insertText("@later:")
+end
